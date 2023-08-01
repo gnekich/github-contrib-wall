aa198e4b5f2fe5e5dfabfe16d4f07798159d2462
Gordan Nekić <gordan@neki.ch>
2023-08-01 21:47:14 +0200

    feat: :art: add new trigger to the workflow on commit to code/feat branch, improve commit generation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Import what we need
 import fs from "fs/promises";
 import { DateTime } from "luxon";
 
@@ -16,6 +17,7 @@ if (process.env.POINTS_TO_PAINT) {
   }
 }
 
+// Default to default value if nothing can be found in the env variable.
 const pointsToPaint = parsedPointsToPaintFromEnv ?? [
   0, 0, 4, 4, 0, 0, 0, 0, 4, 4, 4, 4, 0, 0, 0, 0, 4, 4, 4, 4, 0, 0, 4, 4, 4, 4,
   0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 4, 4,
@@ -34,6 +36,13 @@ const pointsToPaint = parsedPointsToPaintFromEnv ?? [
   0, 0,
 ];
 
+if (!Array.isArray(pointsToPaint)) {
+  throw new Error(
+    "pointsToPaint is not Array! Please check your POINTS_TO_PAINT env variable value"
+  );
+}
+
+// Same for base value if you want to make whole contribution wall green.
 const baseValue = process.env.POINT_BASE_VALUE
   ? parseInt(process.env.POINT_BASE_VALUE, 10)
   : 0;
@@ -45,9 +54,19 @@ const START_DAY = TODAY.minus({ days: pointsToPaint.length - 1 })
   .startOf("week")
   .minus({ days: 1 });
 
+// We need this because last line can be
+const diffInDaysOfStartDayAndToday =
+  parseInt(TODAY.diff(START_DAY, ["days"])?.days, 10) ?? 0;
+
+// Some sanity check in the logs
+console.log("TODAY", TODAY.toISOTime());
+console.log("START_DAY", START_DAY.toISOTime());
+console.log("diffInDaysOfStartDayAndToday", diffInDaysOfStartDayAndToday);
+console.log("size of commits array", pointsToPaint.length);
+
 const commits = [];
-for (let i = 0; i < pointsToPaint.length; i++) {
-  const pointValue = pointsToPaint[i] + baseValue;
+for (let i = 0; i < diffInDaysOfStartDayAndToday; i++) {
+  const pointValue = (pointsToPaint?.[i] ?? 0) + baseValue;
   const dateString =
     START_DAY.plus({ days: i }).toFormat("yyyy-LL-dd") + "T12:00:00";
 
